c216cbe262b34174a25e4e8c08fcc11540711309
Clemeit <tboulan@fastmail.com>
2021-11-07 08:17:45 -0800

    Initial implementation of graphical LFM panel

---
+++ render.js
@@ -0,0 +1,7 @@
+const { MessageAttachment } = require("discord.js");
+const Canvas = require("canvas");
+const lfmSprites = Canvas.loadImage("./img/lfm-sprite.jpg");
+
+function drawLfmPanel() {}
+
+module.exports = { drawLfmPanel };
